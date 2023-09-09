a1c2795c71e032a4bbe2725c736d311364c0f7db
dilidonglong <48240931+dilidonglong@users.noreply.github.com>
2023-09-10 01:37:36 +0800

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-hexo generate
-cp -R public/* .deploy/dilidonglong.github.io
-cd .deploy/dilidonglong.github.io
-git add .
-git commit -m “update”
-git pull --rebase origin master
-git push origin master
